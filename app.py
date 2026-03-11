ee6267890225a60654490bdf3434dd583a45dbee
Talha3507 <talhaisyapar35@gmail.com>
2026-03-11 14:46:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -306,5 +306,5 @@ def logout():
     session.pop("token_info", None)
     return redirect("/")
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+#if __name__ == "__main__":
+ #   app.run(host="127.0.0.1", port=5000, debug=True)
